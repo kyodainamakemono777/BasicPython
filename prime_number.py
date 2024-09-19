92fb348a724d30b5b0e38d857f814659909c837e
kyodainamakemono777 <usamireina0812@gmail.com>
2024-09-20 02:39:16 +0900

    制御構文　問1 素数判定

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,25 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
 
-# TODO
+#2～自然数nまで順重に割っていく　　　　　n÷2, n÷3, n÷4, n÷5, n÷6....    n÷n　
+# 1とnだけでしか割り切れないのであれば『素数』　それ以外は『素数じゃない』にしています
+
+
+#素数判定の関数
+def is_prime(n):
+    list=[1]
+    for i in range(2,n+1):
+        if n%i==0:
+            list.append(i)
+    
+    print("この数は{}で割り切れます".format(list))
+    
+    if len(list)==2:
+        print("この数は素数です")
+    else:
+        print("この数は素数ではありません")
+
+
+a = int(input("aの値を入力: "))
+b = int(input("bの値を入力: "))
+
+is_prime(a)
+is_prime(b)
