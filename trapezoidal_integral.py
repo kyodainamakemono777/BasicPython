771db3234b6b416eb8934e31d696ed867ef3086b
kyodainamakemono777 <usamireina0812@gmail.com>
2024-09-20 22:14:01 +0900

    制御構文問2 台形積分

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,38 @@
 from math import sin
-# --example--
-# print(sin(0))
-# >>> 0
-# -----------
+import math 
+
+
+#aとbの値
+a=0
+b=(1/2)*math.pi
+
+
+
+#aからbの区間をn等分してhを求める （台形の高さ）
+n=100
+h=(b-a)/n
+
+
+
+
+#f(a)からf(b)までの100個のsin関数を当てはめた値を求める　　f(0),f(1),f(2),.....f(100) 　そして100個リストに格納しました
+sin_values=[]
+for x in range(102):
+    split_point=a+(x*h)
+    sin_values.append(sin(split_point))
+
+
+
+
+#求めた間数値で小さい台形の面積を100個求める そして面積リストに追加しました（100個分の台形の面積の値が格納されてます）
+area_list=[]
+for i in range(101):
+    top=(sin_values[i])
+    bottom=(sin_values[i+1])
+    area=((top+bottom)*h)/2
+    area_list.append(area)
+
+
+#100個の台形の面積を全部足し合わせる  近似しました！
+total=sum(area_list)
+print(total)
