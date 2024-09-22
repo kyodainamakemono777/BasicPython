ecd928141d96a5524568b1bce55ee5b29817c8e7
kyodainamakemono777 <usamireina0812@gmail.com>
2024-09-23 01:49:46 +0900

    台形積分 修正しました

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -17,7 +17,7 @@ h=(b-a)/n
 
 #f(a)からf(b)までの100個のsin関数を当てはめた値を求める　　f(0),f(1),f(2),.....f(100) 　そして100個リストに格納しました
 sin_values=[]
-for x in range(102):
+for x in range(n+1):
     split_point=a+(x*h)
     sin_values.append(sin(split_point))
 
@@ -26,7 +26,7 @@ for x in range(102):
 
 #求めた間数値で小さい台形の面積を100個求める そして面積リストに追加しました（100個分の台形の面積の値が格納されてます）
 area_list=[]
-for i in range(101):
+for i in range(n):
     top=(sin_values[i])
     bottom=(sin_values[i+1])
     area=((top+bottom)*h)/2
