b61bebc283b243f263086ec3cbc60444fce1caa2
kyodainamakemono777 <usamireina0812@gmail.com>
2024-09-23 20:01:25 +0900

    問3 ユークリッドの互除法　修正しました

--- euclid.py
+++ euclid.py
@@ -1,18 +1,8 @@
-#大小関係を判定した後、最大公約数を求めています
-
-
-# 【大小判定】２つの値のどちらが大きいか小さいか判定しています　
-def max_min(x,y):
-    if x>y or x==y:
-        a,b=x,y
-        return a,b
-    elif x<y:
-        a,b=y,x
-        return a,b
-
-
 #最大公約数を求める関数
 def find_gcd(a,b):
+    if a<b:
+        a,b=b,a
+
     r=a%b
     if r==0:
         print("最大公約数は",b)
@@ -32,29 +22,22 @@ def find_gcd(a,b):
 
 
 #test　629と259の最大公約数
-a,b=max_min(629,259)
-find_gcd(a,b)
+find_gcd(629,259)
 
 
 #10と20の最大公約数
-a,b=max_min(10,20)
-find_gcd(a,b)
+find_gcd(10,20)
 
 #14と91の最大公約数
-a,b=max_min(14,91)
-find_gcd(a,b)
+find_gcd(14,91)
 
 #91と14の最大公約数
-a,b=max_min(91,14)
-find_gcd(a,b)
+find_gcd(91,14)
 
 
 #input関数を使ったやりかたでも最大公約数を求めることができるようにしました
 a_1=int(input("aの値を入力してください"))
 b_1=int(input("bの値を入力してください"))
 a,b=a_1,b_1
-a,b=max_min(a,b)
 print(a_1,"と",b_1,"の",end=' ')
 find_gcd(a,b)
-
-
