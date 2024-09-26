87fc62d9d8d84f30b02351e3bd94e2a9cc92b9dd
kyodainamakemono777 <usamireina0812@gmail.com>
2024-09-26 21:32:25 +0900

    関数 問3問4 ユークリッドの互除法

--- euclid.py
+++ euclid.py
@@ -5,39 +5,32 @@ def find_gcd(a,b):
 
     r=a%b
     if r==0:
-        print("最大公約数は",b)
+        return b
     else:
         while True:
             a=b
             b=r
             r=(a%b)
             if r==0:
-                print("最大公約数は",b)
+                return b
                 break
 
 
-#一度で割り切れて、余りが0になったら、その数が最大公約数になります
-#一度で割り切れないものは、余りが0になるまで割るのを繰り返します
+#上の関数を使用して、お互いが素であるか判定する関数
+def 素を判定 (a,b):
+    return find_gcd(a,b)==1
 
 
 
-#test　629と259の最大公約数
-find_gcd(629,259)
+
+#2つ値を入力して最大公約数を求める
+a=int(input("aの値を入力してください"))
+b=int(input("bの値を入力してください"))
+result=find_gcd(a,b)
+print(result)
 
 
-#10と20の最大公約数
-find_gcd(10,20)
-
-#14と91の最大公約数
-find_gcd(14,91)
-
-#91と14の最大公約数
-find_gcd(91,14)
+#お互いが素であるかを確認する 素であればTrue 違うとFalse
+print(素を判定(a,result))
 
 
-#input関数を使ったやりかたでも最大公約数を求めることができるようにしました
-a_1=int(input("aの値を入力してください"))
-b_1=int(input("bの値を入力してください"))
-a,b=a_1,b_1
-print(a_1,"と",b_1,"の",end=' ')
-find_gcd(a,b)
