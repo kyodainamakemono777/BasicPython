f440d65868b52d1dcc843b18b1d4869af08c675c
kyodainamakemono777 <usamireina0812@gmail.com>
2024-09-27 23:22:30 +0900

    素数判定　修正しました

--- prime_number.py
+++ prime_number.py
@@ -1,28 +1,16 @@
 #素数判定の関数  
 def is_prime(n):
-    if "." in n:
-        return ("エラー　小数点を含まない数を入力してください")
-    else:
-        n=int(n)
-        if n<1:
-            return ("エラー　０より大きい数を入力してください")
-
-    list=[1]
+    values=[1]
     for i in range(2,n+1):
         if n%i==0:
-            list.append(i)
+            values.append(i)
     
-    if len(list)==2:
+    if len(values)==2:
         return True
     else:
         return False
 
 
-a = input("aの値を入力: ")
+a = int(input("値を入力: "))
 result_a=is_prime(a)
 print(result_a)
-
-
-
-#少数点を含む数や、負の数を入力するとエラーのメッセージが出るようにしました。
-#素数であればTrue 素数じゃなければFalse がでます
