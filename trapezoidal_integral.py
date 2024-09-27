cfb8ce460460a03407fd4f472f10bd0f58b9bee5
kyodainamakemono777 <usamireina0812@gmail.com>
2024-09-27 23:23:53 +0900

    台形積分　修正しました

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -4,17 +4,11 @@ import math
 #台形積分の関数
 def trapezoidal (f, a=0, b=1, n=100):
     h=(b-a)/n
-    values=[]
-    for x in range (n+1):
-        values.append(f(a+(x*h)))
-    
-    area_list=[]
-    for y in range (n):
-        top=values[y]
-        bottom=values[y+1]
-        area_list.append((top+bottom)*h/2)
-    
-    total=sum(area_list)
+    total=0
+    for x in range (n):
+        top=(f(a+x*h))
+        bottom=(f(a+(x+1)*h))
+        total+=((top+bottom)*h/2)
     return total
 
 
@@ -31,3 +25,4 @@ print(trapezoidal(f=func_A, a=0, b=1 ,n=100))
 def func_B(x):
     return math.sqrt(math.pi)*math.exp(-x**2)
 print(trapezoidal(f=func_B, a=-100, b=100, n=1000))
+
